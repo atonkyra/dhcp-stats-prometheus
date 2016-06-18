0087ed3b1868efc2ca7f6795471cfbfb094bfddd
Antti Tönkyrä <daedalus@pingtimeout.net>
2016-06-18 23:10:55 +0300

    add restriction for ipaddrs

--- dhcp-stats-prometheus.py
+++ dhcp-stats-prometheus.py
@@ -1,12 +1,13 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import logging
 import argparse
 import bottle
-from bottle import route, run, template, response
+from bottle import route, run, template, response, request
 import subprocess
 import json
 import sys
 import os
+import ipaddress
 
 logging.basicConfig(
     level=logging.INFO,
@@ -25,16 +26,42 @@ parser.add_argument('-c4', '--dhcp4-config', required=False, help='dhcp4 config 
 parser.add_argument('-c6', '--dhcp6-config', required=False, help='dhcp6 config path', default='/etc/dhcp/dhcpd6.conf')
 parser.add_argument('-l4', '--dhcp4-leases', required=False, help='dhcp4 leases path', default='/var/lib/dhcp/dhcpd.leases')
 parser.add_argument('-l6', '--dhcp6-leases', required=False, help='dhcp6 leases path', default='/var/lib/dhcp/dhcpd6.leases')
+parser.add_argument('-R', '--restrict', required=False, help='restrict metrics to set of IP addresses (may repeat)', default=None, action='append')
 args = parser.parse_args()
 
+restricted_addresses = []
+if args.restrict is not None:
+    for restricted_address in args.restrict:
+        restricted_addresses.append(ipaddress.ip_address(restricted_address))
+
 def exec_command(args):
-    logger.info(' '.join(args))
     p = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     out, err = p.communicate()
     return out.decode('ascii', errors='ignore')
 
+def test_address_pair(a, b):
+    if a.version == 4 and b.version == 6:
+        return (a == b.ipv4_mapped)
+    if a.version == 6 and b.version == 4:
+        return (a.ipv4_mapped == b)
+    return (a == b)
+
+def test_restricted(remote_address):
+    addr = ipaddress.ip_address(remote_address)
+    if args.restrict is not None:
+        allowed = False
+        for restrict_address in restricted_addresses:
+            if test_address_pair(restrict_address, addr):
+                allowed = True
+                break
+        return allowed
+    else:
+        return True
+
 @route('/metrics')
 def prometheus_metrics():
+    if not test_restricted(request['REMOTE_ADDR']):
+        return ''
     dhcpstat = {'shared-networks': []}
     dhcp6stat = {'shared-networks': []}
     try:
